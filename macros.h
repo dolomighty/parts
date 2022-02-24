083f5f13ed2c2072f9fde90e38f14ec8c23ba73b
00519575 <andrealorenzobruno.villa@telecomitalia.it>
2022-02-24 14:31:39 +0100

    .

--- macros.h
+++ macros.h
@@ -1,4 +1,5 @@
 
-#define BIPORAND (1.0-(rand()*2.0/RAND_MAX))
 #define COUNT(ARR) (sizeof(ARR)/sizeof(ARR[0]))
 
+#define UNIRAND  (rand()*1.0/RAND_MAX)
+#define BIPORAND (1.0-rand()*2.0/RAND_MAX)
--- parts.c
+++ parts.c
@@ -3,7 +3,7 @@
 #include <SDL.h>    // HEADER
 
 #include <stdint.h>
-
+#include <time.h>
 
 typedef struct XYf {
   float x,y;
@@ -27,13 +27,13 @@ typedef struct PART {
 // ed anche la dimensione, a questo punto, che potrebbe influenzare
 // quando la repulsione coulombiana interviene. quindi:
 
-#define CLASSI 8
+#define CLASSI 2
 
 
 typedef struct PARTS {
-  float mass;   // f=ma & C.
-  float drag;   // che si applica a spd²
-  float radius;
+//  float mass;   // f=ma & C.
+//  float drag;   // che si applica a spd²
+//  float radius;
   RGB8 rgb;
   int pcount;
   PART *plist;
@@ -41,10 +41,10 @@ typedef struct PARTS {
 } PARTS;
 
 
-// IFNIMPL extern
 PARTS parts[CLASSI];
 
 
+const float EPS  = 1e-5;
 
 // volendo un'approccio statico.
 // se volessimo un approccio dinamico, userei una hash
@@ -67,18 +67,27 @@ PARTS parts[CLASSI];
 #include "main.h"
 
 
+
+
+
+
+
 void parts_init() // HEADER
 {
   const RGB8 rgb[]={
-    {  63 ,  63 ,  63 },
-    {  63 ,  63 , 255 },
-    {  63 , 255 ,  63 },
-    {  63 , 255 , 255 },
-    { 255 ,  63 ,  63 },
-    { 255 ,  63 , 255 },
-    { 255 , 255 ,  63 },
-    { 255 , 255 , 255 },
+    {  63 ,  63 , 255 },    // blu
+    { 255 ,  63 ,  63 },    // red
+    {  63 , 255 ,  63 },    // green
+    { 255 , 128 ,  63 },    // orange
+    {  63 ,  63 ,  63 },    // gray
+    {  63 , 255 , 255 },    // cyan
+    { 255 ,  63 , 255 },    // magen
+    { 255 , 255 ,  63 },    // yel
+    { 255 , 255 , 255 },    // white
   };
+
+  srand(time(0));
+
   int j;
   for( j=0 ; j<CLASSI ; j++ )
   {
@@ -92,9 +101,9 @@ void parts_init() // HEADER
 
     P->rgb = rgb[j];
 
-    P->mass = 1;
-    P->drag = 0.1;
-    P->radius = 1;
+//    P->mass = 1;
+//    P->drag = 0.1;
+//    P->radius = 1;
 
     // genera una matrice di interazione
     memset(P->interact_matrix,0,sizeof(P->interact_matrix));
@@ -105,7 +114,8 @@ void parts_init() // HEADER
     }
 
     // crea le particelle e le posiziona a casaccio
-    P->pcount=200;
+//    P->pcount=10;
+    P->pcount=500;
     int bytes = parts[j].pcount * sizeof(P->plist[0]);
     P->plist = (PART*)malloc(bytes);
     assert(P->plist);
@@ -114,55 +124,86 @@ void parts_init() // HEADER
     for( i=0 ; i<P->pcount ; i++ )
     {
 //      fprintf(stderr,"%d %f %f\n",i,p.x,p.y);
-      P->plist[i].pos.x = (float)rand()*WID/RAND_MAX;
-      P->plist[i].pos.y = (float)rand()*HGT/RAND_MAX;
-      P->plist[i].spd.x = BIPORAND*10;
-      P->plist[i].spd.y = BIPORAND*10;
+//      P->plist[i].pos.x = BIPORAND*100 + WID/2;
+//      P->plist[i].pos.y = BIPORAND*100 + HGT/2;
+//      P->plist[i].spd.x = BIPORAND*10;
+//      P->plist[i].spd.y = BIPORAND*10;
+
+        // mettiamo le particelle in una traiettoria circolare orbitante il centro di massa iniziale
+        P->plist[i].pos.x = BIPORAND*100;
+        P->plist[i].pos.y = BIPORAND*100;
+        float rr = sqrt( P->plist[i].pos.x*P->plist[i].pos.x + P->plist[i].pos.y*P->plist[i].pos.y );
+        float v = sqrt(20/(rr+EPS));
+        P->plist[i].spd.x = -P->plist[i].pos.y*v/(sqrt(rr)+EPS);
+        P->plist[i].spd.y = +P->plist[i].pos.x*v/(sqrt(rr)+EPS);
+
+        P->plist[i].pos.x += WID/2;
+        P->plist[i].pos.y += HGT/2;
     }
   }
+
+    // similars attract, differents repel
+#define I(A,B) parts[A].interact_matrix[B]
+
+    I(0,0) = +1;    I(0,1) = -1;
+    I(1,0) = -1;    I(1,1) = +1;
+
+//    I(0,0) = +1;    I(0,1) = -1;    I(0,2) = -1;
+//    I(1,0) = -1;    I(1,1) = +1;    I(1,2) = -1;
+//    I(2,0) = -1;    I(2,1) = -1;    I(2,2) = +1;
+}
+
+
+
+
+
+// minmax per autoscaling nel rendering
+XYf min;
+XYf max;
+
+static float invlerp( float lo, float hi, float x )
+{
+    return (x-lo)/(hi-lo);
 }
 
 
 static void
 draw_dots( SDL_Renderer *renderer , PARTS *P )
 {
-  assert(P);
-  assert(P->plist);
-  SDL_Point xy[P->pcount];
-  int A;
-  for( A=0 ; A<P->pcount ; A++ )
-  {
-    xy[A].x = P->plist[A].pos.x;
-    xy[A].y = P->plist[A].pos.y;
-//    fprintf(stderr,"%d %d\n",xy[A].x,xy[A].y);
-//    fprintf(stderr,"%d %f %f\n",A,P->plist[A].pos.x,P->plist[A].pos.y);
-  }
-  SDL_SetRenderDrawColor( renderer , P->rgb.r,P->rgb.g,P->rgb.b , 255 );
-  SDL_RenderDrawPoints( renderer , xy , COUNT(xy));
+    assert(P);
+    assert(P->plist);
+    SDL_Point xy[P->pcount];
+    int A;
+    for( A=0 ; A<P->pcount ; A++ )
+    {
+        xy[A].x = invlerp( min.x, max.x, P->plist[A].pos.x )*WID;
+        xy[A].y = invlerp( min.y, max.y, P->plist[A].pos.y )*HGT;
+    }
+    SDL_SetRenderDrawColor( renderer , P->rgb.r,P->rgb.g,P->rgb.b , 255 );
+    SDL_RenderDrawPoints( renderer , xy , COUNT(xy));
 }
 
 
-
-
+ 
 
 
 
 static void
 draw_rects( SDL_Renderer *renderer , PARTS *P )
 {
-  assert(P);
-  assert(P->plist);
-  SDL_Rect xywh[P->pcount];
-  int A;
-  for( A=0 ; A<P->pcount ; A++ )
-  {
-    xywh[A].x = P->plist[A].pos.x-1;
-    xywh[A].y = P->plist[A].pos.y-1;
-    xywh[A].w = 3;
-    xywh[A].h = 3;
-  }
-  SDL_SetRenderDrawColor( renderer , P->rgb.r,P->rgb.g,P->rgb.b , 255 );
-  SDL_RenderFillRects( renderer , xywh , COUNT(xywh));
+    assert(P);
+    assert(P->plist);
+    SDL_Rect xywh[P->pcount];
+    int A;
+    for( A=0 ; A<P->pcount ; A++ )
+    {
+        xywh[A].x = invlerp( min.x, max.x, P->plist[A].pos.x )*WID-1;
+        xywh[A].y = invlerp( min.y, max.y, P->plist[A].pos.y )*HGT-1;
+        xywh[A].w = 3;
+        xywh[A].h = 3;
+    }
+    SDL_SetRenderDrawColor( renderer , P->rgb.r,P->rgb.g,P->rgb.b , 255 );
+    SDL_RenderFillRects( renderer , xywh , COUNT(xywh));
 }
 
 
@@ -172,12 +213,40 @@ draw_rects( SDL_Renderer *renderer , PARTS *P )
 
 void parts_draw( SDL_Renderer *renderer ) // HEADER
 {
-  int C;
-  for( C=0 ; C<COUNT(parts) ; C++ )
-  {
-//    draw_dots(renderer,&parts[C]);
-    draw_rects(renderer,&parts[C]);
-  } 
+    // minmax
+    min.x = min.y = 0;
+    max.x = WID;
+    max.y = HGT;
+    int C;
+    for( C=0 ; C<COUNT(parts) ; C++ )
+    {
+        PARTS *P = &parts[C];
+        int A;
+        for( A=0 ; A<P->pcount ; A++ )
+        {
+            if( min.x > P->plist[A].pos.x ) min.x = P->plist[A].pos.x;
+            if( min.y > P->plist[A].pos.y ) min.y = P->plist[A].pos.y;
+            if( max.x < P->plist[A].pos.x ) max.x = P->plist[A].pos.x;
+            if( max.y < P->plist[A].pos.y ) max.y = P->plist[A].pos.y;
+        }
+    }
+
+    // viewport iniziale per riferimento di scala
+    SDL_SetRenderDrawColor( renderer , 128,128,128, 255 );
+    SDL_Rect xywh;
+    xywh.x = invlerp( min.x, max.x,   0 )*WID;
+    xywh.y = invlerp( min.y, max.y,   0 )*HGT;
+    xywh.w = invlerp( min.x, max.x, WID )*WID - xywh.x;
+    xywh.h = invlerp( min.y, max.y, HGT )*HGT - xywh.y;
+    SDL_RenderDrawRect( renderer , &xywh );
+
+
+    for( C=0 ; C<COUNT(parts) ; C++ )
+    {
+        draw_dots(renderer,&parts[C]);
+//        draw_rects(renderer,&parts[C]);
+    }
+
 }
 
 
@@ -189,6 +258,66 @@ sign( float x )
 }
 
 
+static float 
+delta
+( XYf *D, XYf *A , XYf *B )
+{
+  // calcola il vettore delta AB, in uno spazio toroidale
+  // vengono generati 9 vettori delta (AB e gli alias ABn)
+  // vengono poi confrontati e ritornato il più corto, es.
+  // ___________ __________ __________ 
+  // |       B₁.|       B₂.|       B₃.|
+  // |          |          |          |
+  // |          |          |          |
+  // |          |          |          |
+  // |__________|__________|__________|
+  // |       B₄.|        B.|       B₅.|
+  // |          | .A       |          |
+  // |          |          |          |
+  // |          |          |          |
+  // |__________|__________|__________|
+  // |       B₆.|       B₇.|       B₈.|
+  // |          |          |          |
+  // |          |          |          |
+  // |          |          |          |
+  // |__________|__________|__________|
+  //
+  // il vettore più corto in questo caso è AB₄
+
+  // spazio standard (no toroidale)
+  D->x = B->x - A->x;
+  D->y = B->y - A->y;
+  return D->x*D->x + D->y*D->y;
+
+//  XYf Bn[3*3] = {
+//    { B->x-(WID+20) - A->x, B->y-(HGT+20) - A->y },
+//    { B->x          - A->x, B->y-(HGT+20) - A->y },
+//    { B->x+(WID+20) - A->x, B->y-(HGT+20) - A->y },
+//    { B->x-(WID+20) - A->x, B->y          - A->y },
+//    { B->x          - A->x, B->y          - A->y },
+//    { B->x+(WID+20) - A->x, B->y          - A->y },
+//    { B->x-(WID+20) - A->x, B->y+(HGT+20) - A->y },
+//    { B->x          - A->x, B->y+(HGT+20) - A->y },
+//    { B->x+(WID+20) - A->x, B->y+(HGT+20) - A->y },
+//  };
+//
+//  D->x = Bn[0].x;
+//  D->y = Bn[0].y;
+//  float rr = D->x*D->x + D->y*D->y;
+//
+//  int i;
+//  for( i=1; i<COUNT(Bn); i++ ){
+//    XYf Dn = { Bn[i].x-A->x, Bn[i].y-A->y };
+//    float dd = Dn.x*Dn.x + Dn.y*Dn.y;
+//    if( dd >= rr ) continue;
+//    *D = Dn;
+//    rr = dd;
+//  }
+//
+//  return rr;
+}
+
+
 static void
 integraz_forza( PARTS *C , PART *A )
 {
@@ -196,7 +325,6 @@ integraz_forza( PARTS *C , PART *A )
   // dovute a tutte le altre parts
   // la dinamica avviene in un secondo momento
 
-  const float EPS  = 1e-5;
   const float MAXF = 1e3;
 
   // per ora usiamo l'approccio naif O(n²)
@@ -213,24 +341,41 @@ integraz_forza( PARTS *C , PART *A )
       PART *B = &parts[ci].plist[bi];
       if(B==A)continue; // auto-interazione? no grazie
 
-      XYf d = { A->pos.x-B->pos.x , A->pos.y-B->pos.y };
-      float rr = d.x*d.x+d.y*d.y; // d²
+      XYf d;
+      float rr = delta( &d, &A->pos, &B->pos ); // d²
 
-      // oltre questa dist, non c'è interaz
-      if(rr>10.0*10.0)continue;
+//      // oltre questa dist, non c'è interaz
+//      if(rr>10.0*10.0)continue;
 
-      // quasi https://it.wikipedia.org/wiki/Potenziale_di_Lennard-Jones
-      float r6  = rr*rr*rr; // d²*³=d⁶
-      float r12 = rr*rr*rr*rr*rr*rr; // d²*⁶=d¹²
+//      // sotto questa dist, non c'è interaz
+//      // rende tutto più stabile
+//      // forse mettendo una fn continua per abbassare drasticamente 
+//      // le forze in caso di distanza bassa ... vediamo
+//      if(rr<5.0*5.0)continue; 
 
-// evitiamo div0 e comportamenti brutti
-      float f = 1/(r12+EPS)-1/(rr+EPS);
-      if(f>+MAXF) f=+MAXF; else
-      if(f<-MAXF) f=-MAXF;
+//      float f = 1/(rr+EPS);
+
+      //gnuplot> plot [0:3] [0:7] (1/x**2)*(1-.01/(.01+x**4)),1/x**2
+
+      float range = 0.1;
+      float f = (1/(rr+EPS))*(1-range/(range+rr*rr));
+
+//      // quasi https://it.wikipedia.org/wiki/Potenziale_di_Lennard-Jones
+//      float r6  = rr*rr*rr; // d²*³=d⁶
+//      float r12 = rr*rr*rr*rr*rr*rr; // d²*⁶=d¹²
+//// evitiamo div0 e comportamenti brutti
+//      float f = 1/(r12+EPS)-1/(rr+EPS);
+//      if(f>+MAXF) f=+MAXF; else
+//      if(f<-MAXF) f=-MAXF;
+
+//      // la particella B è di classe ci
+//      // forza semplificata
+//      fsum.x += d.x*f*C->interact_matrix[ci];
+//      fsum.y += d.y*f*C->interact_matrix[ci];
 
       // la particella B è di classe ci
-      fsum.x += d.x*f*C->interact_matrix[ci];
-      fsum.y += d.y*f*C->interact_matrix[ci];
+      fsum.x += d.x/sqrt(rr)*f*C->interact_matrix[ci];
+      fsum.y += d.y/sqrt(rr)*f*C->interact_matrix[ci];
     }
   }
 
@@ -249,13 +394,16 @@ integraz_forza( PARTS *C , PART *A )
 //  fsum.x -= A->spd.x*d*0.001;
 //  fsum.y -= A->spd.y*d*0.001;
 
-  // drag
-  // modellato come ... boh
-  A->spd.x *= 0.99;
-  A->spd.y *= 0.99;
+//  // drag
+//  // modellato come ... boh
+//  A->spd.x *= 0.999;
+//  A->spd.y *= 0.999;
 
-  A->spd.x += fsum.x / C->mass;
-  A->spd.y += fsum.y / C->mass;
+//  A->spd.x += fsum.x / C->mass;
+//  A->spd.y += fsum.y / C->mass;
+
+  A->spd.x += fsum.x;
+  A->spd.y += fsum.y;
 }
 
 
@@ -264,7 +412,7 @@ integraz_forza( PARTS *C , PART *A )
 
 
 
-static void
+static void 
 integraz_forze()
 {
   // per ora usiamo l'approccio naif O(n²)
@@ -285,28 +433,28 @@ integraz_forze()
 
 
 
-void parts_update( float dt )    // HEADER
+void parts_update( float dt ) // HEADER
 {
-  integraz_forze();
+    integraz_forze();
 
-  // integraz posizione
-  int C;
-  for( C=0 ; C<COUNT(parts) ; C++ )
-  {
-    PARTS *P = &parts[C];
-    int A;
-    for( A=0 ; A<P->pcount ; A++ )
+    // integraz posizione
+    int C;
+    for( C=0 ; C<COUNT(parts) ; C++ )
     {
-      P->plist[A].pos.x += P->plist[A].spd.x * dt;
-      P->plist[A].pos.y += P->plist[A].spd.y * dt;
+        PARTS *P = &parts[C];
+        int A;
+        for( A=0 ; A<P->pcount ; A++ )
+        {
+            P->plist[A].pos.x += P->plist[A].spd.x * dt;
+            P->plist[A].pos.y += P->plist[A].spd.y * dt;
 
-      // wrap. uso un playfield allargato, tanto il clipping è gratis
-      if( P->plist[A].pos.x <     -10 ) P->plist[A].pos.x += WID+20;  else
-      if( P->plist[A].pos.x >= WID+10 ) P->plist[A].pos.x -= WID+20;
-      if( P->plist[A].pos.y <     -10 ) P->plist[A].pos.y += HGT+20;  else
-      if( P->plist[A].pos.y >= HGT+10 ) P->plist[A].pos.y -= HGT+20;
+            //      // wrap. uso un playfield allargato, tanto il clipping è gratis
+            //      if( P->plist[A].pos.x <     -10 ) P->plist[A].pos.x += WID+20;  else
+            //      if( P->plist[A].pos.x >= WID+10 ) P->plist[A].pos.x -= WID+20;
+            //      if( P->plist[A].pos.y <     -10 ) P->plist[A].pos.y += HGT+20;  else
+            //      if( P->plist[A].pos.y >= HGT+10 ) P->plist[A].pos.y -= HGT+20;
+        }
     }
-  }
 
 }
 
